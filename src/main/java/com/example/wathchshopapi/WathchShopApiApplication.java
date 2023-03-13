a31f4cad3d89e1d4d436d5d2f34db4c5fdfbee8a
tuandang198 <76816446+tuandang198@users.noreply.github.com>
2023-03-13 20:21:56 +0700

    first commit

---
+++ src/main/java/com/example/wathchshopapi/WathchShopApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.wathchshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = {"com.example.wathchshopapi.Service"})
+public class WathchShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WathchShopApiApplication.class, args);
+    }
+
+}
